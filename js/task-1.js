edd037f9d6f9d894374d77b5c7c537e5edb851b2
kholshevnikova <151967304+kholshevnikova@users.noreply.github.com>
2024-03-15 15:18:25 +0200

    hw04

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,20 @@
-function slugify(title) {
-  const words = title.split(' ');
-  const slug = words.join('-');
-  return slug.toLowerCase();
+function isEnoughCapacity(products, containerSize) {
+  let totalSum = 0;
+  const sums = Object.values(products);
+
+  for (let sum of sums) {
+    totalSum += sum;
+  }
+  if (totalSum <= containerSize) {
+    return true;
+  }
+  return false;
 }
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
